17500929b1e7cdcb779e0b839bedf24f61c952ae
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-12-03 22:51:24 -0300

    :tada: First commit

---
+++ styles/index.ts
@@ -0,0 +1,29 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  styled,
+  getCssText,
+  globalCss
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#FFF',
+
+      red500: '#EF5454',
+
+      purpleCodify500: '#9F6CFD',
+      purpleCodify700: '#322659',
+      purpleCodify900: '#1F0E3F',
+      purpleCodify900B: '#170A2E',
+      
+      blue500: '#2176FF',
+      
+      gray200: '#E2E8F0',
+      gray400: '#A0AEC0',
+      
+      green400: '#48BB78',
+      
+      yellow500: '#FFB83D',
+    }
+  }
+})
