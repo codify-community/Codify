1ddee2e0409b1eca9ae6415c6dc23a19fd3368ff
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-12-07 23:46:53 -0300

    :mag: Improve the SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,17 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
 
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:title" content="Codify Community" />
+        <meta property="og:description" content="Somos uma comunidade de desenvolvedores que tem como objetivo ajudar iniciantes no mundo da programação." />
+        <meta property="og:image" content="/icon.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="60" />
+        <meta property="og:image:height" content="60" />
+        <meta property="og:type" content="website" />
+        <meta property="og:type" content="article" />
+        <meta property="article:published_time" content="date_time" />
+
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} /> 
       </Head>
       <body>
